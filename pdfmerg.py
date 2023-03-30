e105bbe159f30531c29f25cdaf77c7841bad3d37
Izzy Yang <78888775+izzyyang97@users.noreply.github.com>
2023-03-30 05:43:38 -0400

    Update pdfmerg.py

--- pdfmerg.py
+++ pdfmerg.py
@@ -7,5 +7,15 @@ merger = PdfMerger()
 for pdf in pdfs:
     merger.append(pdf)
 
+# If you want to use append method and append certain pages
+# merger = PdfMerger()
+
+# merger.append("one.pdf")
+# merger.append("two.pdf")
+# merger.append("three.pdf", pages=(0, 4, 2))
+
 merger.write("merged.pdf")
 merger.close()
+
+# The example one.pdf and two.pdf are the files that I had in my directory that were named that. 
+# You have to use the correct file name (within the same directory) to merge the pdf files. 
